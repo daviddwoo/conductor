9934f42156de1950e2bd2f4cd05cac486e24f28c
David Woo <dwoo1016@gmail.com>
2022-05-01 21:51:12 -0400

    added express

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-console.log('hello!')
+const app = express();
+
+const init = async() => {
+    try {
+        const PORT = process.env.PORT || 3000;
+        app.listen(PORT, () => console.log(`Listening on port: ${PORT}`))
+    }
+    catch(ex) {
+        console.log(ex);
+    }
+}
+
+init();
